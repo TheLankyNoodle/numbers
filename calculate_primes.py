822eba8dc6fd4ea0e2e949806d4b623595d2ad9f
TheLankyNoodle <jmlancaster96@gmail.com>
2025-12-14 09:36:22 +1100

    bruh, saving before ruining

--- calculate_primes.py
+++ calculate_primes.py
@@ -1,15 +1,59 @@
 import math
 
-def is_prime(number):
+def prep_file(path):
+    with open(path, "r") as file:
+        content = file.read()
+        start_len = len(content) #only write the file again if the content is changed
+
+        #add a couple of primes if there are none
+        if len(content) < 1:
+            content = "2\n3\n5"
+            print("prep_file: adding first three primes")
+
+        #remove trailing newline (if there is one)
+        if content[-1] == '\n':
+            content = content[:-1]
+            print("prep_file: removing trailing newline")
+
+        if len(content) != start_len:
+            with open(path, "w") as file:
+                file.write(content)
+                print("prep_file: writing file with changes")
+
+def proper_prime(number):
+    #NOTE: This is a proper prime number checker, that doesn't use the previously known primes to check.
+
     if number <= 1:
         return False
     if number == 2:
         return True
     if number % 2 == 0:
         return False
-    for i in range(3, math.ceil(math.sqrt(number)), 2):
+
+    for i in range(3, math.ceil(math.sqrt(number)) + 1, 2):
         if number % i == 0:
             return False
     return True
 
+def quick_prime(number, known_primes = []):
+    #NOTE: This is not a true is_prime function, it doesnt check for 0,1,2 because it assumes the file has been prepped to include 2,3,5 as the first primes
+    if number % 2 == 0:
+        return False
+    #TODO: I should only check prime divisors
+    for i in range(3, math.ceil(math.sqrt(number)) + 1, 2):
+        if number % i == 0:
+            return False
+    return True
+
+def write_primes(file_path):
+    prep_file(file_path)
+    with open(file_path, 'r') as file:
+        content = file.read()
+        largest_prime_so_far = int(content.split('\n')[-1])
+     
+    with open(file_path, 'a') as file:
+        for i in range(largest_prime_so_far + 2, largest_prime_so_far + 1000, 2):
+            if quick_prime(i):
+                #print(f"{i} is prime")
+                file.write(f"\n{i}")
 
